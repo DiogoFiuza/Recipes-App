fbe06c700c261b3930baade93bf93c6cf0659778
vinicius-mns <vinicius_mns@hotmail.com>
2021-11-14 10:06:40 -0300

    Refatorando (foodProgress) - Adicionando localStorage com array de progresso

--- src/pages/FoodProgress.jsx
+++ src/pages/FoodProgress.jsx
@@ -6,45 +6,68 @@ import { fetchFoodById } from '../redux/slices/foodRecipesSlice';
 import '../styles/pageProgress.css';
 
 export default function FoodProgress() {
+  const { mealDetail } = useSelector((store) => store.foodRecipes);
+  const storageLocal = localStorage.getItem('inProgressRecipes');
   const history = useHistory();
   const path = history.location.pathname;
   const index = path.split('/')[2];
   const dispatch = useDispatch();
-  const { mealDetail } = useSelector((store) => store.foodRecipes);
-  const ingredients = [];
+  let ingredients = [];
+  // const [checklist, SetChelist] = useState([]);
 
-  const progressIngredient = () => {
-    const obj = mealDetail[0];
-    const maxLength = 8;
-    for (let i = 1; i <= maxLength; i += 1) {
-      if (obj[`strIngredient${i}`] !== '') {
-        ingredients.push({
-          ingredient: obj[`strIngredient${i}`],
-          value: obj[`strMeasure${i}`],
-        });
+  const verification = () => {
+    const createLocalState = () => {
+      const obj = mealDetail[0];
+      const maxLength = 8;
+
+      for (let i = 1; i <= maxLength; i += 1) {
+        if (obj[`strIngredient${i}`] !== '') {
+          ingredients.push({
+            ingredient: obj[`strIngredient${i}`],
+            value: obj[`strMeasure${i}`],
+          });
+        }
+      }
+      if (storageLocal === null) {
+        localStorage.setItem('inProgressRecipes',
+          JSON.stringify({ cocktails: {
+            [index]: ingredients },
+          }));
+      } else {
+        localStorage.setItem('inProgressRecipes',
+          JSON.stringify({ cocktails: {
+            ...JSON.parse(storageLocal).cocktails,
+            [index]: ingredients },
+          }));
+      }
+      // SetChelist(ingredients);
+    };
+
+    if (storageLocal == null) {
+      createLocalState();
+    } else {
+      const idDasComidasNoLocalStorage = Object.keys(JSON.parse(storageLocal).cocktails);
+      if (idDasComidasNoLocalStorage.find((a) => a === index) === index) {
+        ingredients = JSON.parse(storageLocal).cocktails[index];
+      } else {
+        createLocalState();
       }
     }
 
-    const a = ({ target }) => {
-      const filho = document.querySelector(`.id${target.id}`);
-      if (target.checked === true) {
-        filho.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
-      } else {
-        filho.style.textDecoration = 'none solid rgb(0, 0, 0)';
-      }
-    };
-
-    return ingredients.map(({ ingredient, value }, i) => (
-      <div
-        key={ i }
-        className={ `id${i}` }
-        data-testid={ `${index}-ingredient-step` }
-      >
-        <input type="checkbox" onChange={ a } id={ i } />
-        <p>
-          {`${ingredient} - ${value}`}
-        </p>
-      </div>));
+    return (
+      ingredients.map(({ ingredient, value }, i) => (
+        <div
+          key={ i }
+          className={ `id${i}` }
+          data-testid={ `${index}-ingredient-step` }
+        >
+          <input type="checkbox" id={ i } />
+          <p>
+            {`${ingredient} - ${value}`}
+          </p>
+        </div>
+      ))
+    );
   };
 
   useEffect(() => {
@@ -61,7 +84,7 @@ export default function FoodProgress() {
           <button type="button" data-testid="favorite-btn">L</button>
           <p data-testid="recipe-category">{meal.strCategory}</p>
           <ul>
-            {progressIngredient()}
+            {verification()}
           </ul>
           <h3>Istruções</h3>
           <p data-testid="instructions">{meal.strInstructions}</p>
@@ -71,3 +94,49 @@ export default function FoodProgress() {
     </>
   );
 }
+
+// verifica a chave inProgressRecipes no local storage
+// captura um array das chaves das comidas
+// verifica se a chave exite
+// retorna um bool
+
+// --- quando inicia a pagina ---
+// ....... verifica se tem informacao lo local storage .......
+// olha local storage
+// pega informacoes do local storage
+// subistitui o estados
+
+// === quando atualiza a pagina ===
+// mete informaçao no local storage
+// atualiza com a informacoes novas
+
+// function finder({target:{ value }}) {
+//   return times.filter(({title}) => title.toLowerCase().includes(value.toLowerCase())).map((obj) => obj)
+// }
+
+// localStorage.setItem('inProgressRecipes',
+//   JSON.stringify({ cocktails: { ...JSON.parse(storageLocal).cocktails, 53060: 'substitua' } }));
+
+// const a = ({ target }) => {
+//   const filho = document.querySelector(`.id${target.id}`);
+//   const box = document.getElementById(2);
+
+//   if (target.checked === true) {
+//     filho.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+//   } else {
+//     filho.style.textDecoration = 'none solid rgb(0, 0, 0)';
+//   }
+// };
+
+// return ingredients.map(({ ingredient, value }, i) => (
+//   <div
+//     key={ i }
+//     className={ `id${i}` }
+//     data-testid={ `${index}-ingredient-step` }
+//   >
+//     <input type="checkbox" onChange={ a } id={ i } />
+//     <p>
+//       {`${ingredient} - ${value}`}
+//     </p>
+//   </div>
+// ));
