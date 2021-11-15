70b7320f2eec3ca9a006347fbdd4674319f52591
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-11-14 23:48:38 -0300

    Comenta mock dos dados que serão usados em requisitos anteriores

--- src/pages/RecipesMade.jsx
+++ src/pages/RecipesMade.jsx
@@ -5,31 +5,33 @@ import Header from '../components/Header';
 
 const copy = require('clipboard-copy');
 
-const mockDoneRecipes = [
-  {
-    id: '52898', // idMeal
-    type: 'comida', // não vem da api
-    area: 'British', // strArea
-    category: 'Dessert', // strCategory
-    alcoholicOrNot: '', // strAlcoholic
-    name: 'Chelsea Buns', // strMeal
-    image: 'https://www.themealdb.com/images/media/meals/vqpwrv1511723001.jpg', // strDrinkThumb
-    doneDate: '11/11/2021', //  não vem da api
-    tags: ['Bun', 'Baking'], // strTags (só precisa das duas primeiras)
-  },
-  {
-    id: '16986', // idMeal
-    type: 'bebida', // não vem da api
-    area: '', // strArea
-    category: 'Other/Unknown', // strCategory
-    alcoholicOrNot: 'Alcoholic', // strAlcoholic
-    name: 'Bible Belt', // strMeal
-    image:
-      'https://www.thecocktaildb.com/images/media/drink/6bec6v1503563675.jpg', // strDrinkThumb
-    doneDate: '10/10/2020', //  não vem da api
-    tags: [], // strTags (só precisa das duas primeiras)
-  },
-];
+// o Endpoint usado pra colocar a receita no localStora deve ser o por nome:
+// https://www.themealdb.com/api/json/v1/1/search.php?s=
+// const mockDoneRecipes = [
+//   {
+//     id: '52898', // idMeal
+//     type: 'comida', // não vem da api
+//     area: 'British', // strArea
+//     category: 'Dessert', // strCategory
+//     alcoholicOrNot: '', // strAlcoholic
+//     name: 'Chelsea Buns', // strMeal
+//     image: 'https://www.themealdb.com/images/media/meals/vqpwrv1511723001.jpg', // strDrinkThumb
+//     doneDate: '11/11/2021', //  não vem da api
+//     tags: ['Bun', 'Baking'], // strTags (só precisa das duas primeiras)
+//   },
+//   {
+//     id: '16986', // idMeal
+//     type: 'bebida', // não vem da api
+//     area: '', // strArea
+//     category: 'Other/Unknown', // strCategory
+//     alcoholicOrNot: 'Alcoholic', // strAlcoholic
+//     name: 'Bible Belt', // strMeal
+//     image:
+//       'https://www.thecocktaildb.com/images/media/drink/6bec6v1503563675.jpg', // strDrinkThumb
+//     doneDate: '10/10/2020', //  não vem da api
+//     tags: [], // strTags (só precisa das duas primeiras)
+//   },
+// ];
 
 export default function RecipesMade() {
   const title = 'Receitas Feitas';
@@ -150,6 +152,3 @@ export default function RecipesMade() {
     </>
   );
 }
-
-// o Endpoint usado pra colocar a receita no localStora deve ser o por nome:
-// https://www.themealdb.com/api/json/v1/1/search.php?s=
